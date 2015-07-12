4b235ca109e015561b5834e530ffba1ca727693d
underscorediscovery <sven@underscorediscovery.com>
2015-07-11 22:36:56 -0230

    consistency fixes

--- stb/ImageWrite.hx
+++ stb/ImageWrite.hx
@@ -1,17 +1,20 @@
 package stb;
 
+import haxe.io.BytesData;
+
 @:include('./linc_stb_image_write.cpp')
 extern class ImageWrite {
 
-	@:native("linc::stb_image_write::stbi_write_bmp_wrapper")
-	public static function write_bmp(filename:String, x:Int, y:Int, comp:Int, bytes:haxe.io.BytesData, byteOffset:Int, byteLength:Int):Int;
+	@:native("linc::stb_image_write::write_bmp")
+	public static function write_bmp(filename:String, x:Int, y:Int, comp:Int, bytes:BytesData, byteOffset:Int, byteLength:Int):Int;
 
-	@:native("linc::stb_image_write::stbi_write_tga_wrapper")
-	public static function write_tga(filename:String, x:Int, y:Int, comp:Int, bytes:haxe.io.BytesData, byteOffset:Int, byteLength:Int):Int;
+	@:native("linc::stb_image_write::write_tga")
+	public static function write_tga(filename:String, x:Int, y:Int, comp:Int, bytes:BytesData, byteOffset:Int, byteLength:Int):Int;
 
-	@:native("linc::stb_image_write::stbi_write_hdr_wrapper")
-	public static function write_hdr(filename:String, x:Int, y:Int, comp:Int, bytes:haxe.io.BytesData, byteOffset:Int, byteLength:Int):Int;
+	@:native("linc::stb_image_write::write_hdr")
+	public static function write_hdr(filename:String, x:Int, y:Int, comp:Int, bytes:BytesData, byteOffset:Int, byteLength:Int):Int;
 
-	@:native("linc::stb_image_write::stbi_write_png_wrapper")
-	public static function write_png(filename:String, x:Int, y:Int, comp:Int, bytes:haxe.io.BytesData, byteOffset:Int, byteLength:Int, stride_in_bytes:Int):Int;
-}
+	@:native("linc::stb_image_write::write_png")
+	public static function write_png(filename:String, x:Int, y:Int, comp:Int, bytes:BytesData, byteOffset:Int, byteLength:Int, stride_in_bytes:Int):Int;
+
+} //ImageWrite
--- stb/linc_stb_image_write.cpp
+++ stb/linc_stb_image_write.cpp
@@ -2,36 +2,40 @@
 #define _LINC_STB_IMAGE_WRITE_CPP_
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-
 #include "../lib/stb_image_write.h"
+
 #include <hxcpp.h>
 
 namespace linc {
+
     namespace stb_image_write {
-        int stbi_write_bmp_wrapper(char const *filename, int x, int y, int comp, Array<unsigned char> bytes, 
+
+        int write_bmp(char const *filename, int x, int y, int comp, Array<unsigned char> bytes,
         	unsigned int byteOffset, unsigned int byteLength)
 	    {
 	        return stbi_write_bmp(filename, x, y, comp, &bytes[0] + byteOffset);
 	    }
 
-	    int stbi_write_tga_wrapper(char const *filename, int x, int y, int comp, Array<unsigned char> bytes, 
+	    int write_tga(char const *filename, int x, int y, int comp, Array<unsigned char> bytes,
         	unsigned int byteOffset, unsigned int byteLength)
 	    {
 	        return stbi_write_tga(filename, x, y, comp, &bytes[0] + byteOffset);
 	    }
 
-	    int stbi_write_hdr_wrapper(char const *filename, int x, int y, int comp, Array<unsigned char> bytes, 
+	    int write_hdr(char const *filename, int x, int y, int comp, Array<unsigned char> bytes,
         	unsigned int byteOffset, unsigned int byteLength)
 	    {
 	        return stbi_write_hdr(filename, x, y, comp, (float*)(&bytes[0] + byteOffset));
 	    }
 
-	    int stbi_write_png_wrapper(char const *filename, int x, int y, int comp, Array<unsigned char> bytes, 
+	    int write_png(char const *filename, int x, int y, int comp, Array<unsigned char> bytes,
         	unsigned int byteOffset, unsigned int byteLength, int stride_in_bytes)
 	    {
 	        return stbi_write_png(filename, x, y, comp, &bytes[0] + byteOffset, stride_in_bytes);
 	    }
-    }
-}
 
-#endif
+    } //stb_image_write namespace
+
+} //linc
+
+#endif //_LINC_STB_IMAGE_WRITE_CPP_
