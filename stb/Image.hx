455411ff9d8dd673d406c8e21f7de1729fea06e7
underscorediscovery <sven@underscorediscovery.com>
2015-07-22 01:49:33 -0230

    image; block in missing functions and add hdr converter functions

--- stb/Image.hx
+++ stb/Image.hx
@@ -22,22 +22,49 @@ typedef ImageData = {
 @:include('../linc_stb/linc_stb_image.h')
 extern class Image {
 
-    @:native("linc::stb_image::info")
-    static function info(filename:String) : ImageInfo;
+    //info
 
-    @:native("linc::stb_image::info_from_memory")
-    static function info_from_memory(bytes:BytesData, length:Int) : ImageInfo;
+        @:native("linc::stb_image::info")
+        static function info(filename:String) : ImageInfo;
 
-    @:native("linc::stb_image::load")
-    static function load(filename:String, req_comp:Int = 0) : ImageData;
+        @:native("linc::stb_image::info_from_memory")
+        static function info_from_memory(bytes:BytesData, length:Int) : ImageInfo;
 
-    @:native("linc::stb_image::load_from_memory")
-    static function load_from_memory(bytes:BytesData, length:Int, req_comp:Int = 0) : ImageData;
+        // info_from_callbacks
 
-    @:native("linc::stb_image::failure_reason")
-    static function failure_reason() : String;
+    //load
 
-    // static function load_from_callbacks(length:Int, req_comp:Int = 0) : ImageData;
+        @:native("linc::stb_image::load")
+        static function load(filename:String, req_comp:Int = 0) : ImageData;
+
+        @:native("linc::stb_image::load_from_memory")
+        static function load_from_memory(bytes:BytesData, length:Int, req_comp:Int = 0) : ImageData;
+
+        // load_from_callbacks
+
+    //error
+
+        @:native("linc::stb_image::failure_reason")
+        static function failure_reason() : String;
+
+
+    //hdr
+
+        // loadf
+        // loadf_from_memory
+        // loadf_from_callbacks
+
+        @:native("stbi_hdr_to_ldr_gamma")
+        static function hdr_to_ldr_gamma(gamma:Float):Void;
+
+        @:native("stbi_hdr_to_ldr_scale")
+        static function hdr_to_ldr_scale(scale:Float):Void;
+
+        @:native("stbi_ldr_to_hdr_gamma")
+        static function ldr_to_hdr_gamma(gamma:Float):Void;
+
+        @:native("stbi_ldr_to_hdr_scale")
+        static function ldr_to_hdr_scale(scale:Float):Void;
 
 } //Image
 
